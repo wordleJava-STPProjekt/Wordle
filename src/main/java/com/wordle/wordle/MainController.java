ea258f2bbc1a5eda41ef0f970d6dd758836c2c27
DavidMaglica <davidmaglica@protonmail.ch>
2023-01-23 17:54:28 +0100

    As per agreement with teammate, added files modified by me

--- src/main/java/com/wordle/wordle/MainController.java
+++ src/main/java/com/wordle/wordle/MainController.java
@@ -1,14 +1,73 @@
 package com.wordle.wordle;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+import java.security.Key;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
+    private final MainHelp mainHelp = MainHelp.getInstance();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public GridPane gridPane;
+
+    @FXML
+    public GridPane keyboardRow1;
+
+    @FXML
+    public GridPane keyboardRow2;
+
+    @FXML
+    public GridPane keyboardRow3;
+
+    @FXML
+    public ImageView helpIcon;
+
+    @FXML
+    public HBox  titleHBox;
+
+    @FXML
+    public ImageView restartIcon;
+
+    public void createUI() {
+        createGrid();
+        createKeyboard();
+        createTitleHBox();
+    }
+
+    public void createGrid() {
+        mainHelp.createGrid(gridPane);
+    }
+
+    public void createKeyboard() {
+        mainHelp.createKeyboard(keyboardRow1, keyboardRow2, keyboardRow3);
+    }
+
+    public void gridRequestFocus() {
+        gridPane.requestFocus();
+    }
+
+    @FXML
+    protected void onKeyPressed(KeyEvent keyEvent) {
+        mainHelp.onKeyPressed(gridPane, keyboardRow1, keyboardRow2, keyboardRow1, keyEvent);
+    }
+
+    public void getRandomWord() {
+        mainHelp.getRandomWord();
+    }
+
+    public void showHelp() {
+        HelpWindow.display();
+    }
+
+    public void createTitleHBox() {
+        mainHelp.createTitleHBox(titleHBox);
+    }
+
+    public void restart() {
+        mainHelp.restart(restartIcon, gridPane, keyboardRow1, keyboardRow2, keyboardRow3);
     }
 }
